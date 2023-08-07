28646a7273e62a781daa9a4ccc3de85ec6093010
Gabriel sivla <ghaabriieel@gmail.com>
2023-08-07 18:33:26 -0300

    aula04 finalizada

--- aula04/index.js
+++ aula04/index.js
@@ -1,1 +1,8 @@
-console.log("texte")
+console.log("texte");
+// usamos duas barras para fazer comentarios em javascript
+console.log('Hello word!');//Aqui tem outro comentário
+console.log('Hello word new!');
+//console.log('Linha não comentada')
+/*exemplo de cometario em bloco 
+*usado muito para documentação
+*/
