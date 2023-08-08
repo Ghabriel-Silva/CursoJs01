c04f97e9ce29c9ba263b703e4b7c0f5cb7d378be
Gabriel sivla <ghaabriieel@gmail.com>
2023-08-08 20:11:22 -0300

    Aula 06 finalizada

---
+++ aula07/index.js
@@ -0,0 +1,6 @@
+const numero1 = '10';
+const numero2 =  2;
+const valortotal = numero1 * numero2
+let valortotal02 = valortotal + numero1
+console.log('o valor é,', valortotal02)
+console.log(typeof numero1)
