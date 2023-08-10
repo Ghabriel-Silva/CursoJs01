d4fb43d0f5a03663fc10b12f729e8489f4c04732
Gabriel sivla <ghaabriieel@gmail.com>
2023-08-10 10:25:08 -0300

    aula 08

---
+++ aula08/index.js
@@ -0,0 +1,6 @@
+let nome = 'Gabril';
+nome = 'João'; // não podemos redeclarar a mesma variavel utilizando o let.
+var nome1 = 'Gabriel';
+var nome1 = 'Ketlin'; // com var podemos redecarar a mesma variavel
+console.log(nome)
+console.log(nome1)
