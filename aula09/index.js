cc704b196b47c4a9412c806f7a959471ac64f5da
Gabriel sivla <ghaabriieel@gmail.com>
2023-08-10 10:53:49 -0300

    aula 09

---
+++ aula09/index.js
@@ -0,0 +1,9 @@
+const nome = "Gabriel"; //string
+const nome1 = 'Gabriel';//string
+const nome2 = `Gabriel`;//string
+const nun1 = 5; //number
+const nun2 ='1.5'; //number
+let nomeAluno; // undefined --> Não aponta para lugar nenhum na memória e quem vai fazer isto é o propio javascript;
+const sobrenomealuno = null; //Não aponta para lugar nenhum da memória;
+const boolean = true; //Boolean = true, false esse tipo de dados sao lógicos;
+console.log(typeof boolean)
