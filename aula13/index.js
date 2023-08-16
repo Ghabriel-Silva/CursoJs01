6cf0701ca697960a7306448b657cca94005aabeb
Gabriel sivla <ghaabriieel@gmail.com>
2023-08-15 22:00:20 -0300

    Aula 13

---
+++ aula13/index.js
@@ -0,0 +1,20 @@
+ //           012345678
+let umtext = 'ola certo';
+console.log(umtext.charAt(5));
+console.log(umtext.concat(' texto' , ' escrito' , ' aqui' ));
+console.log(umtext+ ' texto escrito aqui');
+console.log(`${umtext} texto escrito aqui`);
+
+console.log(umtext.indexOf('certo')); //Retorna o valor do inicio do indice da palavra desejada.
+console.log(umtext.lastIndexOf('certo')) // Retorna o valor do final do indice da palavra desejada
+console.log(umtext.replace(/e/g, '#')) // substiui a letra desejada por outra desejada.
+console.log(umtext.length) // retorna o numeros de caracteres da string.
+console.log(umtext.slice(1, 4)) //retorna a parte desejada da string.
+console.log(umtext.slice(-2)) //retorna a parte desejada da string, pegando do final.
+console.log(umtext.split(' ')) //Permite separar as palavras das strings.
+console.log(umtext.toUpperCase()) //Trasforma todas palavras em maisculas.
+console.log(umtext.toLowerCase()) //trasforma todas palavras em maisculas. 
+
+
+
+ 
