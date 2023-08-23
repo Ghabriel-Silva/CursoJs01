839714895ca8d703b415ec4de341dffaab36d413
Gabriel sivla <ghaabriieel@gmail.com>
2023-08-23 18:11:07 -0300

    aula 17 function

---
+++ aula17-funcao/index.js
@@ -0,0 +1,10 @@
+/*                              fuction em js
+*  Uma função em JavaScript é um bloco de código nomeado que executa uma tarefa específica quando é chamado.
+*  As funções segue as mesma regras de uma declaração de variavel.
+ */
+
+function saudacao(nome){     //Neste exemplo a funçao saudação tem o parametro 'nome', quando coloco o nome 'gabriel', dentro da função saudação esse valor  vai para fução parametro.
+ console.log(`Bom dia ${nome}`);
+} 
+saudacao('gabriel');
+saudacao('maria')
