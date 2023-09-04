51be6f783334b39f73f75e995e0d1b524f5d62d3
Gabriel sivla <ghaabriieel@gmail.com>
2023-09-04 11:47:45 -0300

    aula 22 operadores lógicos

---
+++ aula23/index.js
@@ -0,0 +1,8 @@
+const and = true && true && true; // me retorna verdadeiro quando todos forem verdadeiros, caso contario false.
+console.log(and)
+
+
+const ou = true || true || false;// me retorna false apernas se os dois forem false, caso contrario true.
+console.log(ou)
+
+const nao = tr
