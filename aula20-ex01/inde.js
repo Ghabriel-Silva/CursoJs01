e212f6376e6796c2cc0efd49d22e3c2357930cfe
Gabriel sivla <ghaabriieel@gmail.com>
2023-09-04 08:38:16 -0300

    exercio finalizado

--- aula20-ex01/inde.js
+++ aula20-ex01/inde.js
@@ -1,9 +1,29 @@
 function meuescopo(){
-const form = document.querySelector('.form')
+   const form =  document.querySelector('.form');
+    const resultado = document.querySelector('.resultado')
 
- function meuevento(evento){
-  evento.preventDefault()
- }
- form.addEventListener('submit', meuevento )
-}
+    const pessoas = [];
+
+    function fuctionqualquer (evento){  
+        evento.preventDefault();
+        const nome = form.querySelector('.nome')
+        const sobrenome = form.querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+      
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value,
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += `<p>nome: ${nome.value}, sobrenome: ${sobrenome.value} peso: ${peso.value} altura: ${altura.value} </p>`
+    };
+
+    form.addEventListener('submit', fuctionqualquer); //Aqui esta acontecendo o seguinte estou falando para minha variavel form adicinar um escutador de evento, isso faz com a função Addeventlistener,  no caso o evento escolhido é o submite('enviar'), para isso também criei uma função chamda 'functionqualquer', essa vai desempenha o papel de previnir o evento de enviar.
+   }
+ 
 meuescopo()
