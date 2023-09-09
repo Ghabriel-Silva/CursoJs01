d952f156b3e77e405182ef9e1a5aeedf6659fab1
Gabriel sivla <ghaabriieel@gmail.com>
2023-09-09 15:49:55 -0300

    aula

--- treinando-fucoes/js/ex05.js
+++ treinando-fucoes/js/ex05.js
@@ -1,5 +1,7 @@
-/*Exercício 4: Função Recursiva
+/*
 Crie uma função chamada fatorial que recebe um número inteiro positivo como argumento e retorna o fatorial desse número. Lembre-se de que o fatorial de um número é o produto de todos os inteiros positivos de 1 até esse número. Use recursão para implementar a função.*/
+
+
 function fatorial(numero){
     return numero**
 }
